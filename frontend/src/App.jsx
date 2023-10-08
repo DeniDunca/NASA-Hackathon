80659db77804a68c6214592516d9eb6c9b3f47db
unknown <razvandarida.rd@gmail.com>
2023-10-08 11:18:28 +0300

    i was joking with 'pr request'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,12 +10,12 @@ const images = [after1, before1]
 const texts = ["Descriere 1", "Descriere 2"]
 
 export default function App() {
+    
     return (
         <main className="bg-blue-100 overflow-hidden min-w-[1500px]">
             <Navbar/>
             <Index/>
             <Modal title="ana are mere"><div>asd</div></Modal>
         </main> 
-        
     )
 }
